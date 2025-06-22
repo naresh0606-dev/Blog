36a931d0ced2881acfcb22a006ffc9f7cd4368d0
naresh0606-dev <naresh05042002@gmail.com>
2025-06-22 18:02:51 +0530

    complete connect to appwrite

---
+++ src/conf/conf.js
@@ -0,0 +1,10 @@
+const conf={
+appwriteUrl :String(import.meta.env.VITE_APPWRITE_URL),
+appwriteDatabaseId:String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+appwriteBucketId:String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+appwriteCollectionId:String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+appwriteProductId:String(import.meta.env.VITE_APPWRITE_PRODUCT_ID)
+};
+
+
+export default conf
